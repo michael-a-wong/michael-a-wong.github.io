bca58b7b5ae3dee5607a5ffab924b77a361ee841
Michael Wong <michael-a-wong@users.noreply.github.com>
2016-12-21 18:04:52 -0800

    update yahoo api

--- weatherApp/weatherWongView.js
+++ weatherApp/weatherWongView.js
@@ -180,7 +180,7 @@ var view = {
 			}
 		}
 
-		scriptText.src = "https://query.yahooapis.com/v1/public/yql?q=select woeid,name,admin1,country  from   geo.places where text='" +searchText+"' & format=json & callback=placeCallback"; 
+		scriptText.src = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" +  searchText  + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=callbackFunction"; 
 		document.body.appendChild(scriptText); 
 	
 	},
